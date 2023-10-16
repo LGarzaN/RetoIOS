ff2965fdb2516bda263c0146241bacab0db5bf73
Luis Eduardo Garza Naranjo <105826525+LGarzaN@users.noreply.github.com>
2023-10-16 15:02:19 -0600

    initial commit

---
+++ RetoIOS/RetoIOS/RetoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoIOSApp.swift
+//  RetoIOS
+//
+//  Created by Luis Eduardo Garza Naranjo on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RetoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
