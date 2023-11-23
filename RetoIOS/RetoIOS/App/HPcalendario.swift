b2b781d0fcb02d303a85410205b2749d8335f58d
Lebrijay02 <juan.lebrija2002@gmail.com>
2023-11-22 18:06:25 -0600

    cal

--- RetoIOS/RetoIOS/App/HPcalendario.swift
+++ RetoIOS/RetoIOS/App/HPcalendario.swift
@@ -62,40 +62,71 @@ struct HPcalendario: View {
                             }
                             Spacer()
                         }
-                        LazyVGrid(columns: columnLayout) {
-                            ForEach(dias, id: \.self) { dia in
-                                Text(dia)
-                                    .bold()
-                            }
-
-                            let firstDayOfMonth = obtenerPrimerDiaDelMes(mes: mesElegido + 1, year: year)
-                            let daysInMonth = obtenerDiasEnMes(mes: mesElegido + 1, year: year)
-                            let emptySlots = Array(0..<firstDayOfMonth)
-                            ForEach(emptySlots, id: \.self) { _ in
-                                Text("")
-                            }
-
-                            ForEach(daysInMonth, id: \.self) { dia in
-                                VStack{
-                                    Rectangle()
-                                        //.frame(width: 54, height: 0.7)
-                                        .frame(width: geo.size.width/7, height: 0.7)
-                                        .foregroundColor(.blu)
-                                    Button(action: {
-                                        mostrarDatos = true
-                                        diaElegido = dia
-                                    }, label: {
-                                        Text("\(dia)")
-                                            .foregroundColor(.primary)
-                                    })
-                                    .sheet(isPresented: $mostrarDatos){
-                                        HPcalendariodatos(selectedDay: diaElegido, selectedMonth: meses[mesElegido], selectedYear: year)
-                                    }
+                        VStack{
+                            LazyVGrid(columns: columnLayout) {
+                                ForEach(dias, id: \.self) { dia in
+                                    Text(dia)
+                                        .bold()
                                 }
-                                .padding(.bottom, 30)
+
+                                let firstDayOfMonth = obtenerPrimerDiaDelMes(mes: mesElegido + 1, year: year)
+                                let daysInMonth = obtenerDiasEnMes(mes: mesElegido + 1, year: year)
+                                let emptySlots = Array(0..<firstDayOfMonth)
+                                ForEach(emptySlots, id: \.self) { _ in
+                                    Text("")
+                                }
+
+                                ForEach(daysInMonth, id: \.self) { dia in
+                                    VStack{
+                                        Rectangle()
+                                            //.frame(width: 54, height: 0.7)
+                                            .frame(width: geo.size.width/7, height: 0.7)
+                                            .foregroundColor(.blu)
+                                        Button(action: {
+                                            mostrarDatos = true
+                                            diaElegido = dia
+                                        }, label: {
+                                            Text("\(dia)")
+                                                .foregroundColor(.primary)
+                                        })
+                                        .sheet(isPresented: $mostrarDatos){
+                                            HPcalendariodatos(selectedDay: diaElegido, selectedMonth: meses[mesElegido], selectedYear: year)
+                                        }
+                                    }
+                                    .padding(.bottom, 30)
+                                }
                             }
+                            .padding(.horizontal)
+                            Spacer()
                         }
-                        .padding(.horizontal)
+                        .frame(height: 424)
+                        HStack(alignment: .center){
+                            Button(action: {
+                                mesElegido -= 1
+                                if mesElegido < 0 {
+                                    mesElegido = meses.count - 1
+                                    year -= 1
+                                }
+                            }) {
+                                Image(systemName: "arrow.left.circle")
+                                    .font(.title)
+                                    .foregroundColor(.blu)
+                            }
+                            .padding(.leading, 30)
+                            Spacer()
+                            Button(action: {
+                                mesElegido += 1
+                                if mesElegido >= meses.count {
+                                    mesElegido = 0
+                                    year += 1
+                                }
+                            }) {
+                                Image(systemName: "arrow.right.circle")
+                                    .font(.title)
+                                    .foregroundColor(.blu)
+                            }
+                            .padding(.trailing, 30)
+                        }
                         Spacer()
                     }
                 }
@@ -141,121 +172,3 @@ struct HPcalendario_Previews: PreviewProvider {
         HPcalendario()
     }
 }
-/*
- import SwiftUI
-
- struct DiaCalendario: Identifiable {
-     var id = UUID()
-     var dia: Int
-     var fecha: Date
- }
-
- struct HPcalendario: View {
-     @State var mostrarDatos = false
-     let columnLayout = Array(repeating: GridItem(.flexible(minimum: 20, maximum: 100)), count: 7)
-     let dias = ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"]
-     let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-
-     @State var mesElegido = Calendar.current.component(.month, from: Date()) - 1
-     @State var year = Calendar.current.component(.year, from: Date())
-     @State var diaElegido = ""
-
-     var body: some View {
-         VStack {
-             Text("Calendario")
-                 .font(.title)
-                 .bold()
-             Rectangle()
-                 .frame(height: 1)
-                 .foregroundColor(.gray)
-                 .padding()
-             VStack {
-                 Text("\(meses[mesElegido]) \(String(format: "%04d", year))")
-                     .font(.title2)
-
-                 LazyVGrid(columns: columnLayout) {
-                     ForEach(dias, id: \.self) { dia in
-                         Text(dia)
-                     }
-
-                     let firstDayOfMonth = obtenerPrimerDiaDelMes(mes: mesElegido + 1, year: year)
-                     let daysInMonth = obtenerDiasEnMes(mes: mesElegido + 1, year: year)
-                     let emptySlots = Array(0..<firstDayOfMonth)
-                     ForEach(emptySlots, id: \.self) { _ in
-                         Text("")
-                     }
-
-                     ForEach(daysInMonth, id: \.self) { dia in
-                         Button("\(dia)"){
-                             mostrarDatos = true
-                             diaElegido = dia
-                         }
-                         .sheet(isPresented: $mostrarDatos){
-                             HPcalendariodatos(selectedDay: diaElegido, selectedMonth: meses[mesElegido], selectedYear: year)
-                         
-                 //            Text(dia)
-                   //              .frame(width: 30, height: 30)
-                     //            .cornerRadius(15)
-                         }
-                     }
-                 }
-                 .padding()
-
-                 Picker("Selecciona un mes", selection: $mesElegido) {
-                     ForEach(0..<12) { index in
-                         Text(meses[index])
-                     }
-                 }
-                 Picker("Selecciona un año", selection: $year) {
-                     ForEach(2000...2030, id: \.self) { year in
-                         Text(String(format: "%04d", year))
-                     }
-                 }
-             }
-         }
-     }
-
-     func obtenerPrimerDiaDelMes(mes: Int, year: Int) -> Int {
-         let calendar = Calendar.current
-         var components = DateComponents()
-         components.year = year
-         components.month = mes
-         components.day = 1
-         let date = calendar.date(from: components)!
-         let firstWeekday = calendar.component(.weekday, from: date)
-         return firstWeekday - 1
-     }
-
-     func obtenerDiasEnMes(mes: Int, year: Int) -> [String] {
-         let calendar = Calendar.current
-         var components = DateComponents()
-         components.year = year
-         components.month = mes
-         _ = calendar.date(from: components)!
-
-         let dateFormatter = DateFormatter()
-         dateFormatter.dateFormat = "d"
-         var daysInMonth = [String]()
-         for day in 1...31 {
-             components.day = day
-             if let dayDate = calendar.date(from: components),
-                calendar.component(.month, from: dayDate) == mes {
-                 daysInMonth.append(dateFormatter.string(from: dayDate))
-             }
-         }
-         return daysInMonth
-     }
- }
-
- struct HPcalendario_Previews: PreviewProvider {
-     static var previews: some View {
-         HPcalendario()
-     }
- }
-
-
-
-
-
-
- */
